5730b3a13661610f1fc849643324f7a14b018ebe
Wenbo Yang <solrex@gmail.com>
2017-11-16 20:14:32 -0800

    Add empty ios version to project.

---
+++ IOS-CipherSpeed/CipherSpeed/ViewController.h
@@ -0,0 +1,17 @@
+//
+//  ViewController.h
+//  CipherSpeed
+//
+//  Created by Wenbo Yang on 2017/11/16.
+//  Copyright © 2017年 Wenbo Yang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+#pragma mark Console
+@property (nonatomic,strong) IBOutlet UITextView *console;
+
+@end
+
---
+++ IOS-CipherSpeed/CipherSpeed/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  CipherSpeed
+//
+//  Created by Wenbo Yang on 2017/11/16.
+//  Copyright © 2017年 Wenbo Yang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
